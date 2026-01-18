808885b8b10c4dc598f55f4fc3e751027e4bd704
jcfullmer <claye@fullmer.pro>
2026-01-18 15:56:38 -0500

    chapter 1 changes. experimented with different ways to upload and save a thumbnail. Current copy saves it in teh asset folder and serves it from there as well.

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +33,57 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+	ContentType := header.Header.Get("Content-Type")
+	if ContentType != "image/jpeg" && ContentType != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "please upload a jpg or png file.", fmt.Errorf("wrong Content-Type"))
+		return
+	}
+	extensions, err := mime.ExtensionsByType(ContentType)
+	if err != nil || len(extensions) == 0 {
+		respondWithError(w, http.StatusBadRequest, "Unsupported file type", err)
+		return
+	}
+	ext := extensions[0]
+	dbVideo, err := cfg.db.GetVideo(videoID)
+	if err == sql.ErrNoRows {
+		respondWithError(w, http.StatusBadRequest, "No video by that ID", err)
+		return
+	} else if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to get video from database", err)
+		return
+	}
+	if userID != dbVideo.UserID {
+		respondWithError(w, http.StatusUnauthorized, "User is not the video owner", fmt.Errorf("User %c is not the video owner.", userID))
+		return
+	}
+	fileName := videoIDString + ext
+	tnFilePath := filepath.Join(cfg.assetsRoot, fileName)
+	thumbnailFile, err := os.Create(tnFilePath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to create thumbnail file", err)
+		return
+	}
+	defer thumbnailFile.Close()
+	io.Copy(thumbnailFile, file)
+	thumbnailurl := fmt.Sprintf("http://localhost:%v/assets/%v", cfg.port, fileName)
+	dbVideo.ThumbnailURL = &thumbnailurl
+	err = cfg.db.UpdateVideo(dbVideo)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to save thumbnail to database", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, dbVideo)
 }
